23e4f745a73f6ff94535d1beab7842fad1ab90ea
kmfiguerrez <kmfiguerrez@gmail.com>
2024-04-28 14:26:54 +0800

    Used electron updater.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const serve = require("electron-serve");
 const path = require("path");
+const { autoUpdater } = require("electron-updater")
+
+// Auto updater flags
+autoUpdater.autoDownload = false
+autoUpdater.autoInstallOnAppQuit = true
 
 const appServe = app.isPackaged ? serve({
   directory: path.join(__dirname, "../out")
@@ -19,7 +24,46 @@ const createWindow = () => {
     appServe(mainWindow).then(() => {
       win.loadURL("app://-");
     });
-  } else {
+
+    // autoUpdater.checkForUpdatesAndNotify()
+
+    // autoUpdater.on('update-available', () => {
+    //   const message = {
+    //     type: 'info',
+    //     buttons: ['Update Now', 'Later'],
+    //     title: 'Update Available',
+    //     detail: 'A new version of the app is available. Do you want to update now?',
+    //   };
+  
+    //   dialog.showMessageBox(mainWindow, message).then((result) => {
+    //     if (result.response === 0) {
+    //       autoUpdater.downloadUpdate();
+    //     }
+    //   });
+    // });
+  
+    // autoUpdater.on('update-downloaded', (updateInfo) => {
+    //   const message = {
+    //     type: 'info',
+    //     buttons: ['Restart', 'Cancel'],
+    //     title: 'Update Downloaded',
+    //     detail: `New version ${updateInfo.version} downloaded. Restart the app to install.`,
+    //   };
+  
+    //   dialog.showMessageBox(mainWindow, message).then((result) => {
+    //     if (result.response === 0) {
+    //       autoUpdater.quitAndInstall();
+    //     }
+    //   });
+    // });
+  
+    // autoUpdater.on('error', (error) => {
+    //   console.error('Error in auto-updater:', error);
+    //   // Handle errors (optional)
+    // });
+
+  } 
+  else {
     mainWindow.loadURL("http://localhost:3000");
     mainWindow.webContents.openDevTools();
     mainWindow.webContents.on("did-fail-load", (e, code, desc) => {
@@ -30,7 +74,14 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   ipcMain.handle('ping', () => 'pong')
+  
   createWindow()
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length == 0) {
+      createWindow()
+    }
+  })
 })
 
 app.on("window-all-closed", () => {
